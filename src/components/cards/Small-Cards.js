e1c2a8274f1fbf435c31cf9c229645d0581828ba
otonye <otonyeamie@gmail.com>
2021-06-30 00:05:51 +0100

    master

--- src/components/cards/Small-Cards.js
+++ src/components/cards/Small-Cards.js
@@ -2,17 +2,26 @@ import {diffDays} from '../../actions/hotels';
 import {useHistory, Link} from 'react-router-dom';
 import {EditOutlined, DeleteOutlined} from '@ant-design/icons';
 
-const SmallCards = ({h, handleHotelDelete = (f) => f}) => {
+const SmallCards = ({h, handleHotelDelete = (f) => f, owner=false, showViewMoreButtom=true}) => {
     const history = useHistory()
     return (
         <>
         <div className='card mb-3'>
             <div className='row no-gutters'>
                 <div className='col-md-4'>
-                    <img src='https://via.placeholder.com/900x500.png?text=MERN+BOOKING'
-                     alt=""
-                     className="card-image img img-fluid"
-                     />
+                   {
+                       h.image && h.image.contentType
+                       ?
+                       <img src={`http://localhost:8000/hotel/image/${h._id}`}
+                           alt=""
+                           className="card-image img img-fluid"
+                       />
+                       :
+                       <img src='https://via.placeholder.com/900x500.png?text=MERN+BOOKING'
+                            alt=""
+                            className="card-image img img-fluid"
+                       />
+                   }
                 </div>
                 <div className='col-md-8'>
                     <div className='card-body'>
@@ -25,12 +34,15 @@ const SmallCards = ({h, handleHotelDelete = (f) => f}) => {
                          </p>
                          <p className='card-text'>Available From: {new Date(h.from).toLocaleDateString()}</p>
                          <button onClick={() => history.push(`/hotel/${h._id}`)} className='btn btn-primary'>Show More</button>
-                         <div className='d-flex justify-content-between h4'>
+                         {
+                             owner && 
+                             <div className='d-flex justify-content-between h4'>
                              <Link to={`/hotel/edit/${h._id}`}>
                                 <EditOutlined className='text-warning'/>
                              </Link>
                              <DeleteOutlined onClick={() => {handleHotelDelete(h._id)}} className='text danger'/>
                          </div>
+                         }
                     </div>
                 </div>
             </div>
